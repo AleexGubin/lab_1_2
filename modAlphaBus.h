4ac80fe170c0a500333f31e1bcb5243378a8ea99
AleexGubin <113512725+AleexGubin@users.noreply.github.com>
2022-09-14 17:45:19 +0300

    Update modAlphaBus.h

--- modAlphaBus.h
+++ modAlphaBus.h
@@ -1,41 +1,18 @@
-#include  "modAlphaBus.h"
-
-using namespace std;
-
-std::wstring modAlphaBus::encrypt(const std::wstring& open_text) // шифрование
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+#include <cctype>
+#include <locale>
+#include <codecvt>
+class modAlphaBus
 {
-    wstring tabl = open_text;
-    int dl, nstrok, index, x;
-    dl = open_text.length(); // введенный текст
-    nstrok = (dl - 1) / key1 + 1; // количество столбцов
-    x = 0;
-    for (int i = key1; i > 0; i--) { // столбцы
-        for (int j = 0; j < nstrok; j++) { // строки
-            index = i+key1*j;
-            if (index-1 < dl) {
-                tabl[x] = open_text[index-1];
-                x++;
-            }
-        }
-    }
-    return tabl;
-}
-
-std::wstring modAlphaBus::decrypt(const std::wstring& cipher_text) // расшифрование
-{
-    wstring tabl = cipher_text;
-    int dl, nstrok, index, x;
-    dl = cipher_text.length();
-    nstrok = (dl - 1) / key1 + 1; // количество столбцов
-    x = 0;
-    for (int i = key1; i > 0; i--) { // столбцы
-        for (int j = 0; j < nstrok; j++) { // строки
-            index = i+key1*j;
-            if (index-1 < dl) {
-                tabl[index-1] = cipher_text[x];
-                x++;
-            }
-        }
-    }
-    return tabl;
-}
+private:
+    int key1; // кол-во столбцов
+public:
+    modAlphaBus()=delete; // запрет конструктора без параметров
+    modAlphaBus(const int& key) :key1(key) {};
+    std::wstring encrypt(const std::wstring& open_text); // зашифрование
+    std::wstring decrypt(const std::wstring& cipher_text); // расшифрование
+};
